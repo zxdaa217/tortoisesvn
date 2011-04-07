zxdaa217/tortoisesvn

commit 62c8eb3fe80e0e57de2270199fec5bd52661332b

First check whether the line is conflicted.
git-svn-id: http://tortoisesvn.googlecode.com/svn/trunk@21121 4cfb86a4-2c7a-11df-bd14-c1e331e6ceac

src/TortoiseMerge/BottomView.cpp
@@ -144,10 +144,13 @@ void CBottomView::UseMyTextBlock()
         bottomstate.linestates[viewLine] = m_pwndBottom->m_pViewData->GetState(viewLine);
         m_pwndBottom->m_pViewData->SetState(viewLine, m_pwndRight->m_pViewData->GetState(viewLine));
         m_pwndBottom->m_pViewData->SetLineEnding(viewLine, m_pwndBottom->lineendings);
-        if (m_pwndRight->m_pViewData->GetState(viewLine) == DIFFSTATE_CONFLICTEMPTY)
-            m_pwndBottom->m_pViewData->SetState(viewLine, DIFFSTATE_CONFLICTRESOLVEDEMPTY);
-        else
-            m_pwndBottom->m_pViewData->SetState(viewLine, DIFFSTATE_CONFLICTRESOLVED);
+        if (m_pwndBottom->IsViewLineConflicted(viewLine))
+        {
+            if (m_pwndLeft->m_pViewData->GetState(viewLine) == DIFFSTATE_CONFLICTEMPTY)
+                m_pwndBottom->m_pViewData->SetState(viewLine, DIFFSTATE_CONFLICTRESOLVEDEMPTY);
+            else
+                m_pwndBottom->m_pViewData->SetState(viewLine, DIFFSTATE_CONFLICTRESOLVED);
+        }
     }
     CUndo::GetInstance().AddState(leftstate, rightstate, bottomstate, m_ptCaretPos);
     m_pwndBottom->SetModified();

commit 2d41458d89a2a0b27144a74fb9938cd06fedbb0b

Fix copy'n'paste bug introduced in r21121.

src/TortoiseMerge/BottomView.cpp
@@ -146,7 +146,7 @@ void CBottomView::UseMyTextBlock()
         m_pwndBottom->m_pViewData->SetLineEnding(viewLine, m_pwndBottom->lineendings);
         if (m_pwndBottom->IsViewLineConflicted(viewLine))
         {
-            if (m_pwndLeft->m_pViewData->GetState(viewLine) == DIFFSTATE_CONFLICTEMPTY)
+            if (m_pwndRight->m_pViewData->GetState(viewLine) == DIFFSTATE_CONFLICTEMPTY)
                 m_pwndBottom->m_pViewData->SetState(viewLine, DIFFSTATE_CONFLICTRESOLVEDEMPTY);
             else
                 m_pwndBottom->m_pViewData->SetState(viewLine, DIFFSTATE_CONFLICTRESOLVED);