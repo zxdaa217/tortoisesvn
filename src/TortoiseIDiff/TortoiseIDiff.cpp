zxdaa217/tortoisesvn

commit 67d704c260b537da42adac1befb1a0392ad67be1

Use empty() instead of size().  Patch by Sup Yut Sum <ch3cooli>.
git-svn-id: http://tortoisesvn.googlecode.com/svn/trunk@23730 4cfb86a4-2c7a-11df-bd14-c1e331e6ceac

src/TortoiseIDiff/TortoiseIDiff.cpp
@@ -82,7 +82,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     std::unique_ptr<CMainWindow> mainWindow(new CMainWindow(hResource));
     mainWindow->SetRegistryPath(_T("Software\\TortoiseSVN\\TortoiseIDiffWindowPos"));
     std::wstring leftfile = parser.HasVal(_T("left")) ? parser.GetVal(_T("left")) : _T("");
-    if ((leftfile.size() == 0)&&(lpCmdLine[0] != 0))
+    if ((!leftfile.empty()) && (lpCmdLine[0] != 0))
     {
         leftfile = lpCmdLine;
     }

src/TortoiseIDiff/MainWindow.cpp
@@ -810,7 +810,7 @@ BOOL CALLBACK CMainWindow::OpenDlgProc(HWND hwndDlg, UINT message, WPARAM wParam
             centeredrect.bottom = centeredrect.top + (childrect.bottom-childrect.top);
             SetWindowPos(hwndDlg, NULL, centeredrect.left, centeredrect.top, centeredrect.right-centeredrect.left, centeredrect.bottom-centeredrect.top, SWP_SHOWWINDOW);
 
-            if (leftpicpath.size())
+            if (!leftpicpath.empty())
                 SetDlgItemText(hwndDlg, IDC_LEFTIMAGE, leftpicpath.c_str());
             SetFocus(hwndDlg);
         }

commit 0552710f2b63af8171c4b2776df8889fc1bb098d

fix a silly typo introduced in r23730 which broke the left image loading for TortoiseIDiff

src/TortoiseIDiff/TortoiseIDiff.cpp
@@ -82,7 +82,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     std::unique_ptr<CMainWindow> mainWindow(new CMainWindow(hResource));
     mainWindow->SetRegistryPath(_T("Software\\TortoiseSVN\\TortoiseIDiffWindowPos"));
     std::wstring leftfile = parser.HasVal(_T("left")) ? parser.GetVal(_T("left")) : _T("");
-    if ((!leftfile.empty()) && (lpCmdLine[0] != 0))
+    if ((leftfile.empty()) && (lpCmdLine[0] != 0))
     {
         leftfile = lpCmdLine;
     }